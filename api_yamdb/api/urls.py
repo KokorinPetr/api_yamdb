8781537dc6fe741e2c11fc358b3f5960a763f6a0
Артем Пилипенко <artem38skull@yandex.ru>
2023-05-14 15:05:49 +0800

    Generics changed to mixins

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,20 +2,24 @@ from django.urls import include, path
 from rest_framework import routers
 
 from api.views import (
-    CategoryDestroyView,
-    CategoryListCreateView,
-    GenreDestroyView,
-    GenreListCreateView,
+    CategoryCreateListDestroyViewSet,
+    GenreCreateListDestroyViewSet,
     TitleViewSet,
 )
 
 v1_router = routers.DefaultRouter()
+v1_router.register(
+    'categories',
+    CategoryCreateListDestroyViewSet,
+    basename='categories',
+)
+v1_router.register(
+    'genres',
+    GenreCreateListDestroyViewSet,
+    basename='genres',
+)
 v1_router.register('titles', TitleViewSet, basename='titles')
 
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
-    path('v1/categories/', CategoryListCreateView.as_view()),
-    path('v1/categories/<slug:slug>/', CategoryDestroyView.as_view()),
-    path('v1/genres/', GenreListCreateView.as_view()),
-    path('v1/genres/<slug:slug>/', GenreDestroyView.as_view()),
 ]
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, generics, viewsets
+from rest_framework import filters, mixins, viewsets
 from rest_framework.pagination import PageNumberPagination
 
 from api.serializers import (
@@ -11,31 +11,30 @@ from api.serializers import (
 from reviews.models import Category, Genre, Title
 
 
-class CategoryListCreateView(generics.ListCreateAPIView):
+class CreateListDestroyViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    pass
+
+
+class CategoryCreateListDestroyViewSet(CreateListDestroyViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     pagination_class = PageNumberPagination
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
-
-
-class CategoryDestroyView(generics.DestroyAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
     lookup_field = 'slug'
 
 
-class GenreListCreateView(generics.ListCreateAPIView):
+class GenreCreateListDestroyViewSet(CreateListDestroyViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     pagination_class = PageNumberPagination
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
-
-
-class GenreDestroyView(generics.DestroyAPIView):
-    queryset = Genre.objects.all()
-    serializer_class = GenreSerializer
     lookup_field = 'slug'
 
 
