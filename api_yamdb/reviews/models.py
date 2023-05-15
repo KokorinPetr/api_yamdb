ee270dc43a71d503a33f06f74bf40ef2179f6f86
KokorinPetr <kokorin.petr@list.ru>
2023-05-15 22:05:54 +1000

    models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,66 +3,6 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class Category(models.Model):
-    name = models.CharField('название', max_length=256)
-    slug = models.SlugField('ключ', unique=True)
-
-    class Meta:
-        verbose_name = 'категория'
-        verbose_name_plural = 'категории'
-
-    def __str__(self):
-        return self.slug
-
-
-class Genre(models.Model):
-    name = models.CharField('название', max_length=256)
-    slug = models.SlugField('ключ', unique=True)
-
-    class Meta:
-        verbose_name = 'жанр'
-        verbose_name_plural = 'жанры'
-
-    def __str__(self):
-        return self.slug
-
-
-class Title(models.Model):
-    name = models.CharField('название', max_length=256)
-    year = models.IntegerField('год выпуска')
-    # rating = models.IntegerField('рейтинг')
-    description = models.TextField('описание')
-    genre = models.ManyToManyField(Genre, through='GenreTitle')
-    category = models.ForeignKey(
-        Category,
-        on_delete=models.SET_NULL,
-        verbose_name='ключ категории',
-        blank=True,
-        null=True,
-    )
-
-    class Meta:
-        verbose_name = 'произведение'
-        verbose_name_plural = 'произведения'
-
-    def __str__(self):
-        return self.name
-
-
-class GenreTitle(models.Model):
-    genre = models.ForeignKey(
-        Genre,
-        on_delete=models.CASCADE,
-    )
-    title = models.ForeignKey(
-        Title,
-        on_delete=models.CASCADE,
-    )
-
-    def __str__(self):
-        return f'{self.genre} {self.title}'
-
-
 class Review(models.Model):
     author = author = models.ForeignKey(
         User,
